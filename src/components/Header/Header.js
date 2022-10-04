1215e13ad14d5fd44ec62ced343ef5721b0a209e
Moathal <moathal.s.kachi@gmail.com>
2022-10-04 12:16:11 +0300

    Add header Structure
    Add styling using styled components
    
    Add Space Hub logo

---
+++ src/components/Header/Header.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {
+  SpaceXNav,
+  SpaceXHeader,
+  ListContainer,
+  List,
+  NavigationLink,
+  Logo,
+  Image,
+} from './Header.styled';
+import SpaceXLogo from '../../assets/planet.png';
+
+const Header = () => (
+  <SpaceXHeader>
+    <Logo>
+      <Image src={SpaceXLogo} />
+      Space Traveler&apos; Hub
+    </Logo>
+    <SpaceXNav>
+      <ListContainer>
+        <List>
+          <NavigationLink to="/" className={({ isActive }) => `${(isActive ? 'active' : '')}`} end>Rockets</NavigationLink>
+        </List>
+        <List>
+          <NavigationLink to="/missions" className={({ isActive }) => `${(isActive ? 'active' : '')}`} end>Missions</NavigationLink>
+        </List>
+        <List>
+          <NavigationLink to="/my-profile" className={({ isActive }) => `${(isActive ? 'active' : '')}`} end>My Profile</NavigationLink>
+        </List>
+      </ListContainer>
+    </SpaceXNav>
+  </SpaceXHeader>
+);
+
+export default Header;
