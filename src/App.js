68b721f34414adfc171191b6f93026266ecfd627
Moathal <moathal.s.kachi@gmail.com>
2022-10-04 12:24:20 +0300

    Fix eslint Error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, /* Route,  Navigate  */ } from 'react-router-dom';
+import { Routes/* , Route,  Navigate  */ } from 'react-router-dom';
 import Header from './components/Header/Header';
 /* import Rockets from './components/Rockets/Rockets'; */
 /* import Missions from './components/Missions/Missions'; */
